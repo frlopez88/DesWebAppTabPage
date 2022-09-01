154e9e358e7899abcbc9444d85b4a597b2627475
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-08-31 20:59:08 -0600

    Primer Commit

---
+++ TabPageEjemplo/TabPageEjemplo/Views/Calculadora.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabPageEjemplo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Calculadora : ContentPage
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void clic7(object sender, EventArgs e)
+        {
+            double valorActual = double.Parse(inputs.Text);
+
+            if (valorActual == 0.0)
+            {
+                inputs.Text = "7";
+            }
+            else {
+
+                inputs.Text = inputs.Text + "7";
+            }
+        }
+
+        private void clic8(object sender, EventArgs e)
+        {
+            double valorActual = double.Parse(inputs.Text);
+
+            if (valorActual == 0.0)
+            {
+                inputs.Text = "8";
+            }
+            else
+            {
+
+                inputs.Text = inputs.Text + "8";
+            }
+        }
+
+        private void clic9(object sender, EventArgs e)
+        {
+            double valorActual = double.Parse(inputs.Text);
+
+            if (valorActual == 0.0)
+            {
+                inputs.Text = "9";
+            }
+            else
+            {
+
+                inputs.Text = inputs.Text + "9";
+            }
+        }
+
+       
+
+        private void clicNumero(object sender, EventArgs e)
+        {
+            Button tmp = (Button)sender;
+            string tmpValText = tmp.Text;
+
+            double valorActual = double.Parse(inputs.Text);
+
+            if (valorActual == 0.0)
+            {
+                inputs.Text = tmpValText;
+            }
+            else
+            {
+
+                inputs.Text = inputs.Text + tmpValText;
+            }
+        }
+
+
+    }
+}
